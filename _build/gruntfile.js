4e70a187796648372c46594ed26dac9d9a654fd2
JP DeVries <johnpaul.devries@gmail.com>
2015-06-27 11:18:50 -0700

    Compile Sass with ilbsass

--- _build/gruntfile.js
+++ _build/gruntfile.js
@@ -316,7 +316,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-bower-task');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	
-	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-sass');
 	
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-clean');
