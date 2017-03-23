f88448909794da1a1438d5e25a8600e6ae11afea
JP DeVries <mail@devries.jp>
2017-03-24 00:30:55 +0100

    fixing <Icon> config

--- client/src/components/Icon.js
+++ client/src/components/Icon.js
@@ -6,7 +6,7 @@ const Icon = (props) => {
   console.log(props);
   return (
     <svg aria-hidden={props.ariaHidden === undefined ? true : props.ariaHidden} className={`icon icon-${props.icon}`}>
-      <use xlinkHref={`${path.join(props.config.assetsBasePath, props.config.iconSVG)}#icon-${props.icon}`}></use>
+      <use xlinkHref={`${path.join(props.config.assetsBasePath || './assets/', props.config.iconSVG || './img/icons.svg')}#icon-${props.icon}`}></use>
     </svg>
   )
 };
