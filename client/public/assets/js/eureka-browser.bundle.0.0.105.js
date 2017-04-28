91b61569a394d9cd469c2af9d05e263e84d5761c
JP DeVries <mail@devries.jp>
2017-04-28 08:54:59 +0200

    object-fit: contain
    Testing #107

--- client/public/assets/js/eureka-browser.bundle.0.0.105.js
+++ client/public/assets/js/eureka-browser.bundle.0.0.105.js
@@ -27378,7 +27378,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	module.exports = {
 		"name": "eureka-browser",
 		"description": "Eureka is a progressively enhanced Media Browser Component.",
-		"version": "0.0.104",
+		"version": "0.0.105",
 		"license": "BSD-3-Clause",
 		"author": {
 			"name": "JP de Vries",
